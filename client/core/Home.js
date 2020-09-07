b495b622c659a993fd533d61eec2ae323fa2d95b
linjiang82 <linjiang82@gmail.com>
2020-09-07 13:21:31 +1000

    Home component

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import unicornbikeImg from "./../assets/images/unicornbike.jpg";
+import Newsfeed from "../post/Newsfeed";
+import FindPeople from "../user/FindPeople";
+import { isAuthenticated } from "../auth/auth-helper";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -24,23 +28,68 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Home = () => {
+const Home = ({ history }) => {
   const classes = useStyles();
+  const [defaultPage, setDefaultPage] = useState(false);
+
+  useEffect(() => {
+    setDefaultPage(isAuthenticated());
+    const unlisten = history.listen(() => {
+      setDefaultPage(isAuthenticated());
+    });
+    return () => {
+      unlisten();
+    };
+  }, []);
+
   return (
-    <Card className={classes.card}>
-      <Typography variant='h6' className={classes.title}>
-        Home Page
-      </Typography>
-      <CardMedia
-        className={classes.media}
-        image={unicornbikeImg}
-        title='Unicorn Bike'></CardMedia>
-      <CardContent>
-        <Typography variant='body2' component='p'>
-          Welcome to the MERN Skeleton home page
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className={classes.root}>
+      {!defaultPage && (
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <Typography variant='h6' className={classes.title}>
+                Home Page
+              </Typography>
+              <CardMedia
+                className={classes.media}
+                image={unicornbikeImg}
+                title='Unicorn Bicycle'
+              />
+              <Typography
+                variant='body2'
+                component='p'
+                className={classes.credit}
+                color='textSecondary'>
+                Photo by{" "}
+                <a
+                  href='https://unsplash.com/@boudewijn_huysmans'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  Boudewijn Huysmans
+                </a>{" "}
+                on Unsplash
+              </Typography>
+              <CardContent>
+                <Typography type='body1' component='p'>
+                  Welcome to the MERN Social home page.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+      {defaultPage && (
+        <Grid container spacing={8}>
+          <Grid item xs={8} sm={7}>
+            <Newsfeed />
+          </Grid>
+          <Grid item xs={6} sm={5}>
+            <FindPeople />
+          </Grid>
+        </Grid>
+      )}
+    </div>
   );
 };
 
