52715e6b25f4c3a662ef52c6151d6aa23366952c
linjiang82 <linjiang82@gmail.com>
2020-11-16 21:03:49 +1000

    conditionally rendering published courses

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import unicornbikeImg from "./../assets/images/unicornbike.jpg";
+import { listPublished } from "../course/api-course";
+import Courses from "../course/Courses";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -26,22 +27,37 @@ const useStyles = makeStyles((theme) => ({
 
 const Home = () => {
   const classes = useStyles();
-  return (
-    <Card className={classes.card}>
-      <Typography variant='h6' className={classes.title}>
-        MERN Classroom
-      </Typography>
-      <CardMedia
-        className={classes.media}
-        image={unicornbikeImg}
-        title='Unicorn Bike'></CardMedia>
-      <CardContent>
-        <Typography variant='body2' component='p'>
-          Welcome to the MERN Classroom home page
+  const [publishedCourses, setPublishedCourses] = useState([]);
+  useEffect(() => {
+    const AbortCtrl = new AbortController();
+    const signal = AbortCtrl.signal;
+    listPublished(signal).then((data) => {
+      if (data && data.error) console.log(data.error);
+      else setPublishedCourses(data);
+    });
+    return () => {
+      AbortCtrl.abort();
+    };
+  }, []);
+  if (publishedCourses.length == 0)
+    return (
+      <Card className={classes.card}>
+        <Typography variant="h6" className={classes.title}>
+          MERN Classroom
         </Typography>
-      </CardContent>
-    </Card>
-  );
+        <CardMedia
+          className={classes.media}
+          image={unicornbikeImg}
+          title="Unicorn Bike"
+        ></CardMedia>
+        <CardContent>
+          <Typography variant="body2" component="p">
+            Welcome to the MERN Classroom home page
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  else return <Courses courses={publishedCourses}></Courses>;
 };
 
 export default Home;
